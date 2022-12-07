2134be2e57c8a7ac4ca2fb7e3df5cad3776c4d62
Jiong Qi <jiongqipan@gmail.com>
2022-12-07 15:18:05 -0500

    sql done

--- import_db.sql
+++ import_db.sql
@@ -1,48 +1,65 @@
 PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    fname VARCHAR(255),
-    lname VARCHAR(255)
-)
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
 CREATE TABLE questions (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     title VARCHAR(255),
     body VARCHAR(255),
-    author VARCHAR(255)
+    author VARCHAR(255),
     FOREIGN KEY (author) REFERENCES users(id)
-)
+);
 CREATE TABLE question_follows (
-    users_id INTEGER PRIMARY KEY
-    questions_id
+    id INTEGER PRIMARY KEY,
+    users_id INTEGER NOT NULL,
+    questions_id INTEGER NOT NULL,
     FOREIGN KEY (users_id) REFERENCES users(id)
     FOREIGN KEY (questions_id) REFERENCES questions(id)
-)
+);
 CREATE TABLE replies (
-    id INTEGER PRIMARY KEY NOT NULL
-    subject_question_id INTEGER NOT NULL
+    id INTEGER PRIMARY KEY,
+    subject_question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    user_reply_id INTEGER,
+    body TEXT NOT NULL,
     FOREIGN KEY (subject_question_id) REFERENCES questions(id)
-    parent_reply_id INTEGER
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
-    user_reply_id INTEGER NOT NULL
     FOREIGN KEY (user_reply_id) REFERENCES users(id)
-    body TEXT NOT NULL
-)
-CREATE TABLE question_likes(
-    user_like_id INTEGER PRIMARY KEY NOT NULL
-    user_id INTEGER NOT NULL
-    FOREIGN KEY (user_id) REFERENCES users(id)
-    question_id INTEGER NOT NULL
+);
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    liker_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (liker_id) REFERENCES users(id)
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
 
 INSERT INTO
     users (fname, lname)
 VALUES
-    ('Adam', 'Smith')
+    ('Adam', 'Smith'),
     ('Jane', 'Doe');
 
 INSERT INTO
     questions (title, body, author)
 VALUES
-    ('What is your name', )
+    ('Question 1', 'What is your name?', (SELECT id FROM users WHERE fname = 'Adam')),
+    ('Question 2', 'What is your gender?', (SELECT id FROM users WHERE fname = 'Jane'));
+
+INSERT INTO 
+    replies (subject_question_id, parent_reply_id, user_reply_id, body)
+VALUES
+    (1, NULL, 1, 'my name is ');
+
+INSERT INTO 
+    question_likes (liker_id, question_id)
+VALUES
+    (1, 1);
